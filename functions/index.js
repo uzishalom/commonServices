d92add1042792e20d6537ea2147d3462d841f637
uzishalom <64908639+uzishalom@users.noreply.github.com>
2020-06-24 10:16:31 +0300

    POST body check

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,31 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const validateEmail = require("./utils/validate-email");
 
 admin.initializeApp();
 
 // Send Email
 // local test: http://localhost:5001/commonservices-a50f5/us-central1/sendMail
 const sendMail = functions.https.onRequest((req, res) => {
-    res.send("Hello From uzi second time")
+
+    if (!("email" in req.body && "subject" in req.body && "message" in req.body)) {
+        res.json({ "err": "Missing Parameters" });
+        return;
+    }
+
+    const { email, subject, message } = req.body;
+
+    if (!validateEmail(email)) {
+        res.json({ "err": "Wrong Email Format" });
+        return;
+    }
+
+    if (subject.length == 0) {
+        res.json({ "err": "The subject is empty" });
+        return;
+    }
+
+    res.send("OK")
 })
 
 module.exports = {
---
+++ functions/utils/validate-email.js
@@ -0,0 +1,7 @@
+const validateEmail = (email) => {
+    const emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegExp.test(String(email).toLowerCase());
+}
+
+module.exports = validateEmail;
+
